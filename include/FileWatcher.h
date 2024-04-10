ff99ca14141c21cc0698b080223174393e986dbe
nikl174 <nstarost81@gmail.com>
2024-04-10 12:02:34 +0200

    added error_struct and necessary functions/ member vars for file FileWatcher

--- include/FileWatcher.h
+++ include/FileWatcher.h
@@ -1,38 +1,75 @@
 #ifndef FILEWATCHER_H
 
 #define FILEWATCHER_H
+#include <atomic>
+#include <cerrno>
 #include <chrono>
 #include <condition_variable>
 #include <filesystem>
+#include <memory>
+#include <mutex>
 #include <optional>
 #include <string>
 #include <sys/inotify.h>
 #include <thread>
 
+#define INOTIFY_BUF_SIZE 4096
+
+/*! \enum file_watch_error
+ *
+ *  errors on starting the filewatcher, wrapping errno-values
+ */
+enum class file_watch_error {
+  error_unknown,
+  error_still_watching,
+  error_read_access = EACCES,
+  error_already_watched = EEXIST,
+  error_path_to_long = ENAMETOOLONG,
+  error_cant_enter_dir = ENOENT,
+  error_to_many_watchers = ENOSPC,
+  success = 0
+};
+
 /*! \class FileWatcher
  *  \brief Asynchronous watcher for file changes
  *
- *  Watches a file for changes and returnes the content if changed.
+ *  Watches a single file for changes and returnes the content if changed.
  *  Uses inotify-events provided by the linux kernel.
  */
 class FileWatcher {
 public:
-  FileWatcher() noexcept(false);
-  int start_watching(std::filesystem::path file);
+  FileWatcher(std::filesystem::path file) noexcept(false);
+  file_watch_error start_watching();
+  file_watch_error start_watching(std::filesystem::path file);
+
+  // blocking
   std::string wait_and_get();
 
+  // not blocking, optional
+  std::optional<std::string> get_when_changed();
+
+  // optional or needed?
   template <class Rep, class Period>
   std::optional<std::string>
   wait_for_and_get(std::chrono::duration<Rep, Period> time);
+  bool stop_watching();
   virtual ~FileWatcher();
 
 protected:
   std::filesystem::path _file_to_watch;
+  std::atomic_bool _watching;
+  std::shared_ptr<std::string> _changed_file_content;
+  std::shared_ptr<std::mutex>
+      _cv_mut; /*<! mutex for the lock in the condition_variable */
   std::shared_ptr<std::condition_variable>
       _notify_waiter; /*!< for notification of the wait_and_get() - caller */
   int _inotify_fd; /*!< file descriptor for the inotify instance used to detect
                       file changes */
+  int _watch_fd;    /*!< file descriptor for the watch instance for _file_to_watch*/
   std::thread _watching_thread;
+
+
+  void file_watch_thread();
 };
 
 #endif /* end of include guard: FILEWATCHER_H */
